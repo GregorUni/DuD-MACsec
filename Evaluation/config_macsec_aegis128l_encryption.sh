5f27bc821f66f140c4a9895e2ea3218f31a5240e
Greggi <gregor.garten@mailbox.tu-dresden.de>
2019-03-20 17:28:03 +0100

    fix

--- Evaluation/config_macsec_aegis128l_encryption.sh
+++ Evaluation/config_macsec_aegis128l_encryption.sh
@@ -1,6 +1,6 @@
 sudo modprobe -r macsec
 sudo modprobe -v macsec
-sudo ip link add link eno1 macsec0 type macsec cipher aegis128l-128
+sudo ip link add link eth0 macsec0 type macsec cipher aegis128l-128
 sudo ip macsec add macsec0 tx sa 0 pn 1 on key 02 09876543210987654321098765432109
 sudo ip macsec add macsec0 rx address b8:27:eb:ed:6c:a3 port 1
 sudo ip macsec add macsec0 rx address b8:27:eb:ed:6c:a3 port 1 sa 0 pn 1 on key 01 12345678901234567890123456789012
