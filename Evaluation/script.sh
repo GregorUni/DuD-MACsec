c4928ff17fc3fe5444fd816cc97377d82931c663
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-03-20 16:24:42 +0000

    test results

--- Evaluation/script.sh
+++ Evaluation/script.sh
@@ -452,13 +452,13 @@ make_info
 #eva $1 "orig-jumbo" 2936 9000 m
 #eva $1 "orig-jumbo-without-encryption" 2936 9000 mw
 #testcases with original macsec
-eva $1 "macsec-aesgcm-we" 1000 1468 mwe
+#eva $1 "macsec-aesgcm-we" 1000 1468 mwe
 eva $1 "macsec-aesgcm-e" 1000 1468 med
-eva $1 "macsec-chachapoly-we" 1000 1468 cwe
+#eva $1 "macsec-chachapoly-we" 1000 1468 cwe
 eva $1 "macsec-chachapoly-e" 1000 1468 mce
 eva $1 "macsec-aegis128l-e" 1000 1468 ae
-eva $1 "macsec-aegis128l-we" 1000 1468 awe
-#eva $1 "macsec-morus640-e" 1000 1468 mme
+#eva $1 "macsec-aegis128l-we" 1000 1468 awe
+eva $1 "macsec-morus640-e" 1000 1468 mme
 #eva $1 "macsec-morus640-we" 1000 1468 mmwe
 # auch noch mit jumbo? also macsec-chachapoy-jumbo 1500,9000 und 2936, 9000?
 # without macsec funktioniert nicht, weil mtu configuration
