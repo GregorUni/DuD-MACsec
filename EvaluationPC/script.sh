1c96c306d20db1a488a5cd32ec8e76a45bf048f3
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-04-09 17:41:36 +0200

    fixes

--- EvaluationPC/script.sh
+++ EvaluationPC/script.sh
@@ -27,7 +27,7 @@ make_info() {
 
   tc qdisc > $INFO_FILE
 	ip link show macsec0 >> $INFO_FILE
-	ip link show enx18d6c70c0a7e>> $INFO_FILE
+	ip link show eno1>> $INFO_FILE
 	#ip macsec show >> $INFO_FILE
 }
 
@@ -35,7 +35,7 @@ make_info() {
 eva_ping() {
   #echo -e "${GREEN}Start RTT Evaluation of $1 with MTU $2${NC}"
   PING_FILE=$EVA_DIR/final-$FPREFIX-$1-$2-ping.txt
-
+	sleep 2
 	# somehow you cant ping with a packet size of 16. There is a bug with the -A optional. anyways macsec only accepts a packet size of min 46
 
         #sudo timeout 360 ping -A $3 -c 50000 -s $((( $2 - 28 ))) # packet sizes to test -> 16 86 214 470 982 1358 1472
@@ -76,7 +76,8 @@ eva_iperf() {
 eva() {
 #frame sizes to test-> 60 128 256 512 1024 1400 1514
 	 echo -e "Erster Parameter:$1 Zweiter:$2 Dritter:$3 Vierter:$4 Fünfter: $5"
-
+sudo ip link set dev eno1 mtu 1514
+ssh root@$REMOTE_IP "sudo ip link set dev eno1 mtu 1514"
 	if [[ $5 == mwe ]]; then #case macsec with aes(gcm) without encryption 
 		IP=$DEST_IP
 		echo -e "Start Test"
@@ -238,16 +239,17 @@ eva() {
 
 
 	else    #case no macsec no encryption
+		IP=169.254.234.92
                 make_info $2 $4
                 eva_ping $2 $4 $REMOTE_IP
                 #sudo ip link set dev eno1 mtu 60
                 #eva_iperf $1 $2 60 $DEST_IP
-		mtu_config_for_iperf3 $1 $2 164 $REMOTE_IP
-		mtu_config_for_iperf3 $1 $2 292 $REMOTE_IP
-		mtu_config_for_iperf3 $1 $2 548 $REMOTE_IP
-		mtu_config_for_iperf3 $1 $2 1060 $REMOTE_IP
-		mtu_config_for_iperf3 $1 $2 1436 $REMOTE_IP
-		mtu_config_for_iperf3 $1 $2 1550 $REMOTE_IP
+		mtu_config_for_iperf3 $1 $2 164 $IP
+		mtu_config_for_iperf3 $1 $2 292 $IP
+		mtu_config_for_iperf3 $1 $2 548 $IP
+		mtu_config_for_iperf3 $1 $2 1060 $IP
+		mtu_config_for_iperf3 $1 $2 1436 $IP
+		mtu_config_for_iperf3 $1 $2 1550 $IP
 
 	fi
 }
@@ -256,13 +258,14 @@ mtu_config_for_iperf3()
 {
 #third value + 36 if the mtu of macsec0 is changed
 echo -e "mtu_config for iperf3"
+ssh root@$REMOTE_IP "sudo ip link set dev eno1 mtu $3"
 sudo ip link set dev eno1 mtu $3
-echo -e "mtu_config for iperf3 after ip link"
-ssh root@$REMOTE_IP "ip link set dev eno1 mtu $3"
-echo -e "mtu_config for iperf3 after ssh"
-sudo ip link set dev macsec0 mtu $((( $3  - 36 ))) #-40 oder -32??
-ssh root@$REMOTE_IP "ip link set dev macsec0 mtu $((( $3  - 36 )))"
+ssh root@$REMOTE_IP "sudo ip link set dev macsec0 mtu $((( $3  - 36 )))"
+sudo ip link set dev macsec0 mtu $((( $3  - 36))) #-40 oder -32??
+sleep 2
 eva_iperf $1 $2 $3 $4
+
+
 }
 
 
@@ -405,8 +408,8 @@ config_macsec_morus640_without_encryption()
 	sudo modprobe -v morus640
 	sudo ip link add link eno1 macsec0 type macsec cipher morus640-128
 	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
-	sudo ip macsec add macsec0 rx address 18:d6:c7:0c:16:82 port 1
-	sudo ip macsec add macsec0 rx address 18:d6:c7:0c:16:82 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1
+	sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
 	sudo ip link set dev macsec0 up
 	sudo ifconfig macsec0 10.10.12.1/24
 sudo ip link set dev macsec0 mtu 1514
@@ -421,8 +424,8 @@ sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) macsec.ko
 	sudo modprobe -v macsec
 	sudo ip link add link eno1 macsec0 type macsec
 	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
-	sudo ip macsec add macsec0 rx address 18:d6:c7:0c:16:82 port 1
-	sudo ip macsec add macsec0 rx address 18:d6:c7:0c:16:82 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1
+	sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
 	sudo ip link set dev macsec0 up
 	sudo ifconfig macsec0 10.10.12.1/24
 sudo ip link set dev macsec0 mtu 1514
@@ -439,8 +442,8 @@ config_macsec_orig_without_encryption()
 	sudo modprobe -v macsec
 	sudo ip link add link eno1 macsec0 type macsec
 	sudo ip macsec add macsec0 tx sa 0 pn 1 on key 01 12345678901234567890123456789012
-	sudo ip macsec add macsec0 rx address 18:d6:c7:0c:16:82 port 1
-	sudo ip macsec add macsec0 rx address 18:d6:c7:0c:16:82 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
+	sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1
+	sudo ip macsec add macsec0 rx address ec:b1:d7:4b:bc:fd port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
 	sudo ip link set dev macsec0 up
 	sudo ifconfig macsec0 10.10.12.1/24
 sudo ip link set dev macsec0 mtu 1514
@@ -464,13 +467,13 @@ make_info
 #eva $1 "orig-jumbo" 2936 9000 m
 #eva $1 "orig-jumbo-without-encryption" 2936 9000 mw
 #testcases with original macsec
-eva $1 "macsec-aesgcm-we" 1000 1468 mwe
-eva $1 "macsec-aesgcm-e" 1000 1468 med
-eva $1 "macsec-chachapoly-we" 1000 1468 cwe
-eva $1 "macsec-chachapoly-e" 1000 1468 mce
-eva $1 "macsec-aegis128l-e" 1000 1468 ae
-eva $1 "macsec-aegis128l-we" 1000 1468 awe
-eva $1 "macsec-morus640-e" 1000 1468 mme
+#eva $1 "macsec-aesgcm-we" 1000 1468 mwe
+#eva $1 "macsec-aesgcm-e" 1000 1468 med
+#eva $1 "macsec-chachapoly-we" 1000 1468 cwe
+#eva $1 "macsec-chachapoly-e" 1000 1468 mce
+#eva $1 "macsec-aegis128l-e" 1000 1468 ae
+#eva $1 "macsec-aegis128l-we" 1000 1468 awe
+#eva $1 "macsec-morus640-e" 1000 1468 mme
 eva $1 "macsec-morus640-we" 1000 1468 mmwe
 # auch noch mit jumbo? also macsec-chachapoy-jumbo 1500,9000 und 2936, 9000?
 # without macsec funktioniert nicht, weil mtu configuration
