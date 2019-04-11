3054baf14503fa3c61cb6752599b2ef8080e54dc
GregorUni <38833793+GregorUni@users.noreply.github.com>
2019-04-11 13:19:43 +0200

    Delete .macsec.ko.cmd

--- macsec/orig/.macsec.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/test1/DuD-MACsec/macsec/orig/macsec.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/test1/DuD-MACsec/macsec/orig/macsec.ko /home/test1/DuD-MACsec/macsec/orig/macsec.o /home/test1/DuD-MACsec/macsec/orig/macsec.mod.o ;  true
