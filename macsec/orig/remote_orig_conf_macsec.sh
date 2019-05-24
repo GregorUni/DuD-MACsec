e0396e9b32c62d6e5c6d4a10e1be5ed5b590fe38
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-05-24 15:33:47 +0200

    .

---
+++ macsec/orig/remote_orig_conf_macsec.sh
@@ -0,0 +1,14 @@
+#compiling the original macsec.c and insert it into the kernel
+sudo modprobe -r macsec	
+sudo make -C /lib/modules/$(uname -r)/build M=$(pwd) macsec.ko
+sudo cp macsec.ko /lib/modules/$(uname -r)/kernel/drivers/net	
+sudo modprobe -v macsec
+#iproute2 config
+sudo ip link add link $1 macsec0 type macsec
+sudo ip macsec add macsec0 tx sa 0 pn 1 on key 02 09876543210987654321098765432109
+sudo ip macsec add macsec0 rx address $2 port 1
+sudo ip macsec add macsec0 rx address $2 port 1 sa 0 pn 1 on key 01 12345678901234567890123456789012
+sudo ip link set dev macsec0 up
+sudo ifconfig macsec0 $3/24
+sudo ip link set dev macsec0 mtu 1514
+sudo ip link set macsec0 type macsec encrypt $4
