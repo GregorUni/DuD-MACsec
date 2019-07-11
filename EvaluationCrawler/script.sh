ff60a69bdb735d91237f30dbf912286427d2b649
GregorUni <38833793+GregorUni@users.noreply.github.com>
2019-07-11 19:34:04 +0200

    Update script.sh

--- EvaluationCrawler/script.sh
+++ EvaluationCrawler/script.sh
@@ -8,17 +8,17 @@ NC='\033[0m' # No Color
 
 #variables for host_pc
 Host_PTH="~" #folder in which the git repository is located
-HOST_MAC_ADR="3c:d9:2b:02:92:0d" #mac adress
+HOST_MAC_ADR="00:15:17:d9:99:e9" #mac adress
 SOURC_IP=10.10.12.1 #name of ethernet interface
-HOST_ETHERNET_NAME="eth0"
+HOST_ETHERNET_NAME="enp2s0f1"
 
 #variables for remote_pc
 Remote_PTH="~" #folder in which the git repository is located
-Remote_MAC_ADR="3c:d9:2b:02:94:66" #mac adress
+Remote_MAC_ADR="00:15:17:d9:99:a7" #mac adress
 DEST_IP=10.10.12.2 #macsec ip
 REMOTE_IP=141.76.55.43 #internet ip
-ETHERNET_IP=11.11.11.14 #ethernet ip
-REMOTE_ETHERNET_NAME="eth0" #name of ethernet interface
+ETHERNET_IP=11.11.11.13 #ethernet ip
+REMOTE_ETHERNET_NAME="enp2s0f1" #name of ethernet interface
 
 #Cipher configs for iproute2
 AEGIS="aegis128l-128"
