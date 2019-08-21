66b1f58e467639a8112f32056a777fcff58c855d
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-08-21 14:31:12 +0200

    adding linux script

---
+++ Linux/build.sh
@@ -0,0 +1,4 @@
+sudo cp arch/arm/boot/dts/*.dtb /boot/
+sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
+sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
+sudo cp arch/arm/boot/zImage /boot/$KERNEL.img
