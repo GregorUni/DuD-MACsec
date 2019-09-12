400d34e8b89e0d4920736dea79d4f0d68ac0a302
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-09-12 16:25:43 +0200

    fix scripts for test

--- Debug/script2.sh
+++ Debug/script2.sh
@@ -1,8 +1,8 @@
 sudo modprobe -r macsec
 sudo modprobe -v macsec
-sudo ip link add link eth1 macsec0 type macsec
+sudo ip link add link eth0 macsec0 type macsec
 sudo ip macsec add macsec0 tx sa 0 pn 1 on key 02 09876543210987654321098765432109
-sudo ip macsec add macsec0 rx address 18:d6:c7:0c:0a:7e port 1
-sudo ip macsec add macsec0 rx address 18:d6:c7:0c:0a:7e port 1 sa 0 pn 1 on key 01 12345678901234567890123456789012
+sudo ip macsec add macsec0 rx address b8:27:eb:12:24:7e port 1
+sudo ip macsec add macsec0 rx address b8:27:eb:12:24:7e port 1 sa 0 pn 1 on key 01 12345678901234567890123456789012
 sudo ip link set dev macsec0 up
 sudo ifconfig macsec0 10.10.12.2/24
