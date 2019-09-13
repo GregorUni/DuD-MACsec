81b366678a7c1802081eb61d83f0ef3480b55b22
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-09-13 18:37:18 +0200

    test

--- EvaluationPi/script.sh
+++ EvaluationPi/script.sh
@@ -62,12 +62,12 @@ eva_ping() {
         #sudo timeout 360 ping -A $3 -c 50000 -s $((( $2 - 28 ))) # packet sizes to test -> 16 86 214 470 982 1358 1472
 	#sudo timeout 360 ping -A $3 -c 50000 -s $((( 16 - 8 )))   # cause of a bug you have to configure the packet size this way
 	dstat -N $HOST_ETHERNET_NAME,macsec0--noheaders --output $EVA_DIR/ping-$FPREFIX-$1-$2-dstat.csv > /dev/null 2>&1 &	
-	sudo timeout 120 ping -A $3 -c 50000 -s $((( 106 - 28 ))) >> $PING_FILE
-	sudo timeout 120 ping -A $3 -c 50000 -s $((( 234 - 28 ))) >> $PING_FILE
-	sudo timeout 120 ping -A $3 -c 50000 -s $((( 490 - 28 ))) >> $PING_FILE
-	sudo timeout 120 ping -A $3 -c 50000 -s $((( 1002 - 28 ))) >> $PING_FILE
-	sudo timeout 120 ping -A $3 -c 50000 -s $((( 1378 - 28 ))) >> $PING_FILE
-	sudo timeout 120 ping -A $3 -c 50000 -s $((( 1488 - 28 ))) >> $PING_FILE #somehow this doesnt work(maybe the packetsize is to big for the mtu?)
+	sudo timeout 12 ping -A $3 -c 5 -s $((( 106 - 28 ))) >> $PING_FILE
+	sudo timeout 12 ping -A $3 -c 5 -s $((( 234 - 28 ))) >> $PING_FILE
+	sudo timeout 12 ping -A $3 -c 5 -s $((( 490 - 28 ))) >> $PING_FILE
+	sudo timeout 12 ping -A $3 -c 5 -s $((( 1002 - 28 ))) >> $PING_FILE
+	sudo timeout 12 ping -A $3 -c 5 -s $((( 1378 - 28 ))) >> $PING_FILE
+	sudo timeout 12 ping -A $3 -c 5 -s $((( 1464 - 28 ))) >> $PING_FILE #somehow this doesnt work(maybe the packetsize is to big for the mtu?)
 	kill `ps -ef | grep dstat | grep -v grep | awk '{print $2}'`
 }
 
