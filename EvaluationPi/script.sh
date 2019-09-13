6569b944d526fd90a4cf4d2720a611ed0f27ee4e
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-09-13 16:13:58 +0200

    fixing interfaces again

--- EvaluationPi/script.sh
+++ EvaluationPi/script.sh
@@ -14,7 +14,7 @@ HOST_ETHERNET_NAME="eth2"
 
 #variables for remote_pc
 Remote_PTH=/home/pi #folder in which the git repository is located
-Remote_MAC_ADR="18:d6:c7:0c:16:82" #mac adress
+Remote_MAC_ADR="18:d6:c7:0c:16:d3" #mac adress
 DEST_IP=10.10.12.2 #macsec ip
 REMOTE_IP=141.76.55.43 #internet ip
 ETHERNET_IP=169.254.61.115 #ethernet ip
