bb175c0e5c3129e544cc378aa2bdb8e13f9aaf90
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2019-10-24 13:47:54 +0200

    pushing test results

--- EvaluationPi/remote_config_macsec.sh
+++ EvaluationPi/remote_config_macsec.sh
@@ -6,5 +6,5 @@ sudo ip macsec add macsec0 rx address $3 port 1
 sudo ip macsec add macsec0 rx address $3 port 1 sa 0 pn 1 on key 02 09876543210987654321098765432109
 sudo ip link set dev macsec0 up
 sudo ifconfig macsec0 $4/24
-sudo ip link set dev macsec0 mtu 1514
+#sudo ip link set dev macsec0 mtu 1514
 sudo ip link set macsec0 type macsec encrypt $5
