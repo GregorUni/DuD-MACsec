9ee7d521429d89bfa9b3fdb93e8840392b692e6b
GregorUni <gregor.garten@mailbox.tu-dresden.de>
2020-01-06 17:21:48 +0100

    little changes

--- EvaluationPi4/script.sh
+++ EvaluationPi4/script.sh
@@ -8,17 +8,17 @@ NC='\033[0m' # No Color
 
 #variables for host_pc
 Host_PTH="~" #folder in which the git repository is located
-HOST_MAC_ADR="18:d6:c7:0c:16:d3" #mac adress
+HOST_MAC_ADR="18:d6:c7:0c:16:82" #mac adress
 SOURC_IP=10.10.12.1 #name of ethernet interface
-HOST_ETHERNET_NAME="eth1"
+HOST_ETHERNET_NAME="eth2"
 
 #variables for remote_pc
 Remote_PTH=/home/pi #folder in which the git repository is located
-Remote_MAC_ADR="ec:08:6b:1c:25:04" #mac adress
+Remote_MAC_ADR="dc:a6:32:18:b2:56" #mac adress
 DEST_IP=10.10.12.2 #macsec ip
-REMOTE_IP=141.76.55.43 #internet ip
-ETHERNET_IP=169.254.244.105 #ethernet ip
-REMOTE_ETHERNET_NAME="eth1" #name of ethernet interface
+REMOTE_IP=141.76.55.44 #internet ip
+ETHERNET_IP=192.168.1.20 #ethernet ip
+REMOTE_ETHERNET_NAME="eth0" #name of ethernet interface
 
 #Cipher configs for iproute2
 AEGIS="aegis128l-128"
